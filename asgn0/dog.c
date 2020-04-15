f392d48f896f9d5eaa62cfc6d94dc5e7d00c29ae
Eric Tang <etang@Erics-iMac.local>
2020-04-14 17:45:18 -0700

    fixed up dog.c

--- asgn0/dog.c
+++ asgn0/dog.c
@@ -11,42 +11,59 @@
 #include <err.h>
 
 
-static void write_to(ssize_t b, uint8_t *buffer) {
-	while((b = read(STDIN_FILENO, buffer, sizeof(buffer)) > 0)) {
-		b = write(STDOUT_FILENO, buffer, b);
+static void read_and_write(uint8_t *buffer, char args[]) {
+	while(1) {
+		ssize_t bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
+		if(bytes_read < 0){
+			warn("%s", args);
+			break;
+		}
+		if(bytes_read == 0){
+			break;
+		}
+		ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		if(bytes_written < 0){
+			warn("%s", args);
+		}
 	}
 }
 
 int main(int argc, char* argv[]) {
-
 	uint8_t buf[BUF_LEN];
-	ssize_t bytes = 0;
-
 	if(argc == 1) {
-		write_to(bytes, buf);
+		read_and_write(buf, argv[0]);
 		return 0;
 	}
 	for(int i = argc - 1; i > 0; i--) {
 		if(!strcmp(argv[i], "-")) {
-			write_to(bytes, buf);
+			read_and_write(buf, argv[i]);
 			i--;
 		}
 		if(i == 0) {
 			return 0;
 		}
-		uint8_t fd = open(argv[i], O_RDONLY);
+		ssize_t fd = open(argv[i], O_RDONLY);
 		if( fd < 0) {
 			warn("%s", argv[i]);
+
 		}
-		while((bytes = read(fd, buf, sizeof(buf))) > 0) {
-			if(bytes < 0) {
-				warn("%s", argv[i]);
+		while(1){
+			ssize_t bytes_read = read(fd, buf, sizeof(buf));
+			if(bytes_read < 0) {
+				//warn("%s", argv[i]);
+				break;
+			} else if(bytes_read == 0){
 				break;
 			}
-			bytes = write(STDOUT_FILENO, buf, bytes);
+			ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+			if(bytes_written < 0){
+				warn("%s",argv[i]);
+			}
 		}
 		close(fd);
 	}
+	return 0;
 }
+
 
 
