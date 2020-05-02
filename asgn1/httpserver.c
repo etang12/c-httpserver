5b43ad3f38cd91fca1813779f0b71e811fbeb596
Eric Tang <etang@Erics-iMac.local>
2020-05-02 12:58:02 -0700

    httpserver.c fixed

--- asgn1/httpserver.c
+++ asgn1/httpserver.c
@@ -11,6 +11,7 @@
 #include <stdbool.h> // true, false
 #include <inttypes.h>
 #include <err.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 4096
 #define BODY_BUFFER_SIZE 32768
@@ -51,29 +52,42 @@ void read_http_request(ssize_t client_sockd, struct httpObject* message) {
     
     recv(client_sockd, message->buffer, sizeof(message->buffer), 0);
     //printf("%s", message->buffer);
-    char* body_check = strstr((char*)&message->buffer, "\r\n\r\n");
+    //char* body_check = strstr((char*)&message->buffer, "\r\n\r\n");
+    //printf("%s\n", body_check);
     char* token = strtok((char*)&message->buffer, "\r\n");
     //printf("this is first token: %s\n", token);
     sscanf(token, "%s %s %s", message->method, message->filename, message->httpversion);
+    if(strlen(message->filename) > 28){
+        message->status_code = 400;
+        dprintf(client_sockd, "%s %d Bad Request\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+        return;
+    }
+    int len = strspn(message->filename ,"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_/");
+    //printf("len: %d\n", len);
+    if(strlen(message->filename) != len){       //check for valid chars in resource name
+        message->status_code = 400;
+        dprintf(client_sockd, "%s %d Bad Request\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+        return;
+    }
     memmove(message->filename, (message->filename)+1, strlen(message->filename));
     //printf("%s %s %s\n", message->method, message->filename, message->httpversion);
     
     //printf("0x%" PRIXPTR "\n", (uintptr_t) ((body_in_header)));
     //printf("this is strstr len: %d\n", strlen(body_check);
-    if(strlen(body_check) >= 4){        //check if request contains header and body (bad request)
+    /*if(strlen(body_check) > 4){        //check if request contains header and body (bad request)
+        printf("strlen: %lu\n", strlen(body_check));
         //uint8_t responseBuffer[BUFFER_SIZE];
         message->status_code = 400;
         //printf("no body in header, good request\n");
-        //send(client_sockd, "HELLO CLIENT\n" , sizeof("HELLO CLIENT"), 0);
-        //send response 400 bad request and close socket
-    }
+        printf("send response 400 bad request and close socket\n");
+        dprintf(client_sockd, "%s %d Bad Request\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+        
+    }*/
         
     if(strlen(token) > BUFFER_SIZE){
-        //printf("need to throw error where header length > 4096 bytes");
         message->status_code = 400;
-        dprintf(client_sockd, "%s %d Bad Request\n", message->httpversion, message->status_code);
-        close(client_sockd);
-        exit(EXIT_FAILURE);
+        dprintf(client_sockd, "%s %d Bad Request\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+        return;
     }
     while(token != NULL){
         //printf("token1: %s\n", token);
@@ -90,16 +104,6 @@ void read_http_request(ssize_t client_sockd, struct httpObject* message) {
         token = strtok(NULL, "\r\n");
     }
     //printf("%s %s %s\n Content-Length: %zu\n", message->method, message->filename, message->httpversion, message->content_length);
-    /*if(message->content_length > BODY_BUFFER_SIZE){     //checks if content length is larger than 32 KiB, is so throw error
-        printf("400 Bad Request, body exceeds 32KiB\n");
-        message->status_code = 400;
-        dprintf(client_sockd, "%s %d Bad Request\nContent-Length: %zd\n", message->httpversion, message->status_code, message->content_length);
-        close(client_sockd);
-        exit(EXIT_FAILURE);
-    }*/
-    //printf("outside of loop: %s\n", token);
-    
-    //printf("%s %s %s\n Content-Length: %zu\n", message->method, message->filename, message->httpversion, message->content_length);
 
     /*
      * Start constructing HTTP request based off data from socket
@@ -113,118 +117,143 @@ void read_http_request(ssize_t client_sockd, struct httpObject* message) {
     \brief 2. Want to process the message we just received
 */
 //function created to check if a file exists to determine status code (200 or 201 during a PUT request)
-bool file_exists (char *filename) {
+int if_exists (char *filename){
   struct stat statbuf;   
-  return (stat (filename, &statbuf) == 0);
+  return (stat (filename, &statbuf));
 }
 
+int get_file_size (char *filename){
+    struct stat statbuf;
+    stat(filename, &statbuf);
+    return statbuf.st_size;
+}
+//HANDLE PUT REQUEST
 void process_request(ssize_t client_sockd, struct httpObject* message) {
     //printf("%" PRIu8 "\n", message->buffer[0]);
     //printf("process recv: %zd\n", bytes);
     //printf("inside process_request\n");
     uint8_t body_buffer[BODY_BUFFER_SIZE];
+    int file_exists = if_exists(message->filename);
     if(strcmp(message->method, "PUT") == 0){
-        if(file_exists(message->filename)){
+        if(file_exists == 0){
             message->status_code = 200;
             //printf("file exists\n");
         } else {
             message->status_code = 201;
-            //printf("file doesn't exist\n");
         }
         ssize_t putfd = open(message->filename, O_CREAT|O_WRONLY|O_TRUNC, 0644);       //creates a new file if it doesn't exist, overwrites it if it does
+        //printf("putfd: %zd\n", putfd);
         if(putfd < 0){
-            warn("%s", message->filename);
-            exit(EXIT_FAILURE);
+            //printf("errno=%d\n", errno);
+            if(errno == EACCES){
+                message->status_code = 403;
+                dprintf(client_sockd, "%s %d Forbidden\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            }
+            return;
         }
         ssize_t bytes_recv = recv(client_sockd, body_buffer, BODY_BUFFER_SIZE, 0);
         //printf("first bytes_recv: %zu\n", bytes_recv);
-        if(bytes_recv == 0){
-            write(putfd, body_buffer, bytes_recv);
-            //printf("inside == 0\n");
-            dprintf(client_sockd, "%s %d OK\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
-            close(putfd);
-            //printf("end of this\n");
-            return;
-        } else if(bytes_recv <= BODY_BUFFER_SIZE){       //check if bytes_recv is less than buffer size
-            int temp_bytes_recv = bytes_recv;            //temp variable created to keep track of bytes recv after different iterations of loop 
+        if(bytes_recv <= BODY_BUFFER_SIZE){       //check if bytes_recv is less than buffer size
+            int cont_len = message->content_length;     
             while(true){
+                cont_len = cont_len - bytes_recv;       //decrement cont_len by number of bytes recv after each iteration to handle any sized files
                 //printf("in loop\n");
-                write(putfd, body_buffer, bytes_recv);         //writes contents of body from request into the created file which is saved in the server
-                if(bytes_recv == message->content_length){
+                ssize_t bytes_written = write(putfd, body_buffer, bytes_recv);         //writes contents of body from request into the created file which is saved in the server
+                if(bytes_written < 0){
+                    message->status_code = 500;
+                    dprintf(client_sockd, "%s %d Internal Server Error\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+                }
+                if(cont_len == 0){
                     //printf("breaking out loop\n");
                     break;
                 }
-                if(temp_bytes_recv == message->content_length - bytes_recv){       //temp bytes == different iteration loop recv value, 
-                    //printf("bytes_recv subtraction: %zd\n", bytes_recv);
-                    //printf("break loop\n");
-                    break;
-                }
                 bytes_recv = recv(client_sockd, body_buffer, BODY_BUFFER_SIZE, 0);
-                //printf("count: %d bytes_recv: %zu\n", bytes_recv);
+                //printf("second bytes_recv: %zu\n", bytes_recv);
             }
+            //printf("outside loop\n");
+            //printf("code: %d\n", message->status_code);
             if(message->status_code == 200){
                 dprintf(client_sockd, "%s %d OK\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
             } else if(message->status_code == 201){
-                dprintf(client_sockd, "%s %d Created\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+                dprintf(client_sockd, "%s %d Created\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
             }
+            //printf("hello\n");
             close(putfd);
             return;
         } 
     }
+    //HANDLE GET REQUEST
     else if(strcmp(message->method, "GET") == 0){
+        int file_size = get_file_size(message->filename);
         //printf("%s\n", message->filename);
+        //printf("content-length: %zd\n", message->content_length);
         ssize_t getfd = open(message->filename, O_RDONLY);
         //printf("getfd: %d\n", getfd);
         if(getfd < 0){                                  //if file doesn't exist, then 404 request (Not Found)
             message->status_code = 404;
-            dprintf(client_sockd, "%s %d Not Found\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            if(errno == EACCES){
+                message->status_code = 403;
+                dprintf(client_sockd, "%s %d Forbidden\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            } else {
+                dprintf(client_sockd, "%s %d Not Found\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            }
+            
             //printf("error opening file in GET\n");
             return;
         }
         //printf("opened in GET successfully, FD = %zu\n", getfd);
         ssize_t readfd = read(getfd, body_buffer, BODY_BUFFER_SIZE);
         if(readfd < 0){
-            printf("error reading file in GET\n");
+            message->status_code = 500;
+            dprintf(client_sockd, "%s %d Internal Server Error\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
             return;
         }
         message->status_code = 200;
+        dprintf(client_sockd, "%s %d OK\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, file_size);
+        int cont_len = file_size;     
+        while(true){
+            cont_len = cont_len - readfd;                      //decrement cont_len by number of bytes recv after each iteration to handle any sized files
+            ssize_t bytes_written = write(client_sockd, body_buffer, readfd);         //writes contents of body from request into the created file which is saved in the server
+            if(bytes_written < 0){
+                message->status_code = 500;
+                dprintf(client_sockd, "%s %d Internal Server Error\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            }
 
-        dprintf(client_sockd, "%s %d OK\r\nContent-Length: %zd\r\n\r\n", message->httpversion, message->status_code, readfd);
-        send(client_sockd, body_buffer, readfd, 0);
+            if(cont_len == 0){
+                break;
+            }
+            readfd = read(getfd, body_buffer, BODY_BUFFER_SIZE);
+            //printf("second bytes_recv: %zu\n", bytes_recv);
+        }
         close(getfd);
     }
+    //HANDLE HEAD REQUEST
     else if(strcmp(message->method, "HEAD") == 0){
+    int file_size = get_file_size(message->filename);
     ssize_t headfd = open(message->filename, O_RDONLY);
         //printf("getfd: %d\n", getfd);
         if(headfd < 0){                                  //if file doesn't exist, then 404 request (Not Found)
             message->status_code = 404;
-            dprintf(client_sockd, "%s %d Not Found\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
-            //printf("error opening file in GET\n");
+            if(errno == EACCES){
+                message->status_code = 403;
+                dprintf(client_sockd, "%s %d Forbidden\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            } else {
+                dprintf(client_sockd, "%s %d Not Found\r\nContent-Length: %d\r\n\r\n", message->httpversion, message->status_code, 0);
+            }
             return;
         }
-        //printf("opened in GET successfully, FD = %zu\n", getfd);
         ssize_t readfd = read(headfd, body_buffer, BODY_BUFFER_SIZE);
         if(readfd < 0){
             printf("error reading file in GET\n");
             return;
         }
-        message->content_length = readfd;
+        message->content_length = file_size;
         message->status_code = 200;
         dprintf(client_sockd, "%s %d OK\r\nContent-Length: %zd\r\n\r\n", message->httpversion, message->status_code, message->content_length);
         close(headfd);
     }
     return;
 }
-
-/*
-    \brief 3. Construct some response based on the HTTP request you recieved
-*/
-void construct_http_response(ssize_t client_sockd, struct httpObject* message) {
-   //printf("Constructing Response\n");
-
-    return;
-}
-
 
 int main(int argc, char** argv) {
     /*
@@ -293,32 +322,9 @@ int main(int argc, char** argv) {
          * 2. Read HTTP Message
          */
         read_http_request(client_sockd, &message);
-        /*
-         * 3. Process Request
-         */
         process_request(client_sockd, &message);
-        //printf("hello");
 
-        /*
-         * 4. Construct Response
-         */
-        construct_http_response(client_sockd, &message);
-
-        /*
-         * 5. Send Response
-         */
-        //printf("Response Sent\n");
-
-        /*
-         Sample Example which wrote to STDOUT once.
-         
-        uint8_t buff[BUFFER_SIZE + 1];
-        ssize_t bytes = recv(client_sockd, buff, BUFFER_SIZE, 0);
-        buff[bytes] = 0; // null terminate
-        printf("[+] received %ld bytes from client\n[+] response: \n", bytes);
-        write(STDOUT_FILENO, buff, bytes);
-        */
-       close(client_sockd);
+        close(client_sockd);
     }
 
     return EXIT_SUCCESS;
